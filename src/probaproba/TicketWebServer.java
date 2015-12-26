ab6d7095ac0670be72b60cf1a49348e45878d669
MiryamSubiza <miryam_247@hotmail.com>
2015-12-26 01:00:55 +0100

    Avansando con el html wey

--- src/probaproba/TicketWebServer.java
+++ src/probaproba/TicketWebServer.java
@@ -9,6 +9,9 @@ import GSILabs.BModel.Artist;
 import GSILabs.BModel.Concert;
 import GSILabs.BModel.Exhibition;
 import GSILabs.BModel.FechaCompleta;
+import GSILabs.BModel.Festival;
+import GSILabs.BSystem.BussinessSystem;
+import GSILabs.BTesting.P01Tester;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,7 +25,9 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class TicketWebServer {
+    
     private static final int PORT = 8080;
+    
     public static void main(String[] args) {
         try {
             ServerSocket server = new ServerSocket(PORT);
@@ -40,12 +45,18 @@ public class TicketWebServer {
         }
     }
 }
+
 class ThreadSocket extends Thread {
+    
     private Socket clientSocket;
+    private ToHTML toHTML = new ToHTML();
+    private BussinessSystem bs = P01Tester.getBussinessSystem();
+    
     ThreadSocket(Socket clientSocket) {
         this.clientSocket = clientSocket;
         this.start();
     }
+    
     @Override
     public void run() {
         try {
@@ -66,50 +77,55 @@ class ThreadSocket extends Thread {
                 // Le paso la subsecuencia que contiene unicamente el dominio que me pide el cliente
                 // de modo que lo analizo y separo en trozos para poder ver en más profundidad que me
                 // el cliente que le devuelva
-                System.out.println(request_method.substring(5, request_method.length() - 8));
-                Map <String,String> parms = queryToMap(request_method.substring(5, request_method.length() - 8));
+                System.out.println(request_method.substring(5, request_method.length() - 9));
+                Map <String,String> parms = queryToMap(request_method.substring(5, request_method.length() - 9));
                 System.out.println("parms es " + parms);
+                if ((parms.size() == 1)) {
+                    if (parms.containsKey(""))
+                        out.println(toHTML.listOfEvents());
+                    else if (parms.containsKey("concerts"))
+                        out.println(toHTML.listOfConcerts(bs.getConcerts()));
+                    else if (parms.containsKey("exhibitions"))
+                        out.println(toHTML.listOfExhibitions(bs.getExhibitions()));
+                    else if (parms.containsKey("festivals"))
+                        out.println(toHTML.listOfFestivals(bs.getFestivals()));
+                }
+                else if ((parms.size() == 2)) {
+                    
+                }
             }
             
             System.out.println("1.HTTP-HEADER: " + line);
             line = "";
-            // looks for post data
-            /*int postDataI = -1;
-            while ((line = in.readLine()) != null && (line.length() != 0)) {
-                System.out.println("2.HTTP-HEADER: " + line);
-                if (line.indexOf("Content-Length:") > -1) {
-                    postDataI = new Integer(line.substring(line.indexOf("Content-Length:") + 16,line.length())).intValue();
-                }
-            }
-            String postData = "";
-            // read the post data
-            if (postDataI > 0) {
-                char[] charArray = new char[postDataI];
-                in.read(charArray, 0, postDataI);
-                postData = new String(charArray);
-            }*/
-            out.println("HTTP/1.0 200 OK");
+            
+            /*out.println("HTTP/1.0 200 OK");
             out.println("Content-Type: text/html; charset=utf-8");
-            out.println("Server: MINISERVER");
+            out.println("Server: MINISERVER");*/
             // this blank line signals the end of the headers
-            out.println("");
+            //out.println("");
             // Send the HTML page
-            out.println("<H1>List of events</H1>");
-            out.println("<H2>Request Method->" + request_method + "</H2>");
-            /*out.println("<H2>Post->" + postData + "</H2>");*/
+            //out.println("<H1>List of events</H1>");
+            //out.println("<H2>Request Method->" + request_method + "</H2>");
+            //out.println("<H2>Post->" + postData + "</H2>");
             //out.println("<form name=\"input\" action=\"form_submited\" method=\"post\">");
             //out.println("Username: <input type=\"text\" name=\"user\"><input type=\"submit\" value=\"Submit\"></form>");
-            out.println("<H3><a href='/concerts'>Concerts</a></H3>");
-            out.println("<H3><a href='/exhibitions'>Exhibitions</a></H3>");
-            out.println("<H3><a href='/festivals'>Festivals</a></H3>");
+            //out.println("<H3><a href='/concerts'>Concerts</a></H3>");
+            //out.println("<H3><a href='/exhibitions'>Exhibitions</a></H3>");
+            //out.println("<H3><a href='/festivals'>Festivals</a></H3>");
             out.close();
             clientSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
     public static Map<String, String> queryToMap (String query) {
         Map<String, String> result = new HashMap<String, String>();
+        String[] queries;
+        if (query.contains("/")) {
+            queries = query.split("/");
+            query = queries[1];
+        }
         for (String param : query.split("&")) {
             String pair[] = param.split("=");
             if (pair.length>1) {
@@ -120,46 +136,5 @@ class ThreadSocket extends Thread {
             }
         }
         return result;
-    }
-    
-    public String concertToHTML (Concert c) {
-        String html;
-        html = "<html><head>Concert</head><body>Name: " + c.getName() + "\nPerformer: ";
-        if (c.getPerformer() instanceof Artist)
-            html.concat("<a href='/artist?artistName=" + c.getPerformer().getName() + "'>" 
-                    + c.getPerformer().getName() + "</a>\n");
-        else
-            html.concat("<a href='/collective?collectiveName=" + c.getPerformer().getName() 
-                    + "'>" + c.getPerformer().getName() + "</a>\n");
-        html.concat("Date: " + ((FechaCompleta)c.getStartDate()).fechaToString() + "\nDoor opening: " 
-                + ((FechaCompleta)c.getDoorOpeningTimeConcert()).horaToString() + "h\nStart time: " 
-                + ((FechaCompleta)c.getStartTimeConcert()).horaToString() + "h\nClosing time: " 
-                + ((FechaCompleta)c.getClosingTimeConcert()).horaToString() + "h\nLocation: " 
-                + "<a href='/location?locationName=" + c.getLocation().getName() + "'>" + 
-                c.getLocation().getName() + "</a>\n" + "</body></html>");
-        return html;
-    }
-    
-    public String exhibitionToHTML (Exhibition e) {
-        String html;
-        html = "<html><head>Exhibition</head><body>Name: " + e.getName() + "\nTitle: "
-                + e.getTitle() + "\nOrganizer: " + e.getOrganizerName() + "Start date: " 
-                + ((FechaCompleta)e.getStartDate()).fechaToString() + "\nStart time: " 
-                + ((FechaCompleta)e.getStartTimeExhibition()).horaToString() + "h\nClosing time: " 
-                + ((FechaCompleta)e.getClosingTimeExhibition()).horaToString() + "h\nClosing date: "
-                + ((FechaCompleta)e.getEndingDate()).fechaToString();
-        if (e.getPerformer() instanceof Artist)
-            html.concat("<a href='/artist?artistName=" + e.getPerformer().getName() + "'>" + 
-                    e.getPerformer().getName() + "</a>\nWeb links: ");
-        else
-            html.concat("<a href='/collective?collectiveName=" + e.getPerformer().getName() + "'>" 
-                    + e.getPerformer().getName() + "</a>\nWeb links: ");
-        Iterator i = e.getWebLinks().iterator();
-        while (i.hasNext()) {
-            html.concat(i.next() + "  ");
-        }
-        html.concat("\nLocation: <a href='/location?locationName=" + e.getLocation().getName() + "'>" + 
-                e.getLocation().getName() + "</a>\n</body></html>");
-        return html;
     }
 }
---
+++ src/probaproba/ToHTML.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probaproba;
+
+import GSILabs.BModel.Artist;
+import GSILabs.BModel.Concert;
+import GSILabs.BModel.Exhibition;
+import GSILabs.BModel.FechaCompleta;
+import GSILabs.BModel.Festival;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ *
+ * @author mirya
+ */
+public class ToHTML {
+    
+    public String listOfEvents () {
+        return ("<html><meta http-equiv=\'Content-Type\' content=\'text/html; charset=UTF-8\' />"
+                + "<head><h1>List of events</h1></head><body>" + 
+                "<h3><a href='/concerts'>Concerts</a></h3>" +
+                "<h3><a href='/exhibitions'>Exhibitions</a></h3>" + 
+                "<h3><a href='/festivals'>Festivals</a></h3></body></html>");
+    }
+    
+    public String listOfConcerts (HashMap concerts) {
+        String html = "<html><meta http-equiv=\'Content-Type\' content=\'text/html; charset=UTF-8\' />"
+                + "<head><h1>List of concerts</h1></head><body><ul>";
+        Iterator i = concerts.values().iterator();
+        while (i.hasNext()) {
+            String cName = ((Concert)i.next()).getName();
+            html = html.concat("<li><a href='concert?concertName=" + quitarEspacios(cName) + "'>" + 
+                    cName + "</a></li>");
+        }
+        html = html.concat("</ul></body></html>");
+        return html;
+    }
+    
+    public String listOfExhibitions (HashMap exhibitions) {
+        String html = "<html><meta http-equiv=\'Content-Type\' content=\'text/html; charset=UTF-8\' />"
+                + "<head><h1>List of exhibitions</h1></head><body><ul>";
+        Iterator i = exhibitions.values().iterator();
+        while (i.hasNext()) {
+            String eName = ((Exhibition)i.next()).getName();
+            html = html.concat("<li><a href='exhibition?exhibitionName=" + quitarEspacios(eName)
+                    + "'>" + eName + "</a></li>");
+        }
+        html = html.concat("</ul></body></html>");
+        return html;
+    }
+    
+    public String listOfFestivals (HashMap festivals) {
+        String html = "<html><meta http-equiv=\'Content-Type\' content=\'text/html; charset=UTF-8\' />"
+                + "<head><h1>List of festivals</h1></head><body><ul>";
+        Iterator i = festivals.values().iterator();
+        while (i.hasNext()) {
+            String fName = ((Festival)i.next()).getName();
+            html = html.concat("<li><a href='festival?festivalName=" + quitarEspacios(fName)
+                    + "'>" + fName + "</a></li>");
+        }
+        html = html.concat("</ul></body></html>");
+        return html;
+    }
+    
+    public String concertToHTML (Concert c) {
+        String html;
+        html = "<html><meta http-equiv=\'Content-Type\' content=\'text/html; charset=UTF-8\' />"
+                + "<head><h1>Concert</h1></head><body>Name: " + c.getName() + "<br>Performer: ";
+        if (c.getPerformer() instanceof Artist)
+            html = html.concat("<a href='/artist?artistName=" + quitarEspacios(c.getPerformer().getName())
+                    + "'>" + c.getPerformer().getName() + "</a><br>");
+        else
+            html = html.concat("<a href='/collective?collectiveName=" + quitarEspacios(c.getPerformer().getName()) 
+                    + "'>" + c.getPerformer().getName() + "</a><br>");
+        html = html.concat("Date: " + ((FechaCompleta)c.getStartDate()).fechaToString() + 
+                "<br>Door opening: " + ((FechaCompleta)c.getDoorOpeningTimeConcert()).horaToString() 
+                + "h<br>Start time: " + ((FechaCompleta)c.getStartTimeConcert()).horaToString() + 
+                "h<br>Closing time: " + ((FechaCompleta)c.getClosingTimeConcert()).horaToString() + 
+                "h<br>Location: " + "<a href='/location?locationName=" + quitarEspacios(c.getLocation().getName())
+                + "'>" + c.getLocation().getName() + "</a><br>" + "</body></html>");
+        return html;
+    }
+    
+    public String exhibitionToHTML (Exhibition e) {
+        String html;
+        html = "<html><meta http-equiv=\'Content-Type\' content=\'text/html; charset=UTF-8\' />"
+                + "<head><h1>Exhibition</h1></head><body>Name: " + e.getName() + "<br>Title: "
+                + e.getTitle() + "<br>Organizer: " + e.getOrganizerName() + "<br>Start date: " 
+                + ((FechaCompleta)e.getStartDate()).fechaToString() + "<br>Start time: " 
+                + ((FechaCompleta)e.getStartTimeExhibition()).horaToString() + "h<br>Closing time: " 
+                + ((FechaCompleta)e.getClosingTimeExhibition()).horaToString() + "h<br>Closing date: "
+                + ((FechaCompleta)e.getEndingDate()).fechaToString() + "<br>";
+        if (e.getPerformer() instanceof Artist)
+            html = html.concat("<a href='/artist?artistName=" + quitarEspacios(e.getPerformer().getName())
+                    + "'>" + e.getPerformer().getName() + "</a><br>Web links: ");
+        else
+            html = html.concat("<a href='/collective?collectiveName=" + quitarEspacios(e.getPerformer().getName())
+                    + "'>" + e.getPerformer().getName() + "</a><br>Web links: ");
+        Iterator i = e.getWebLinks().iterator();
+        while (i.hasNext()) {
+            html = html.concat(i.next() + "  ");
+        }
+        html = html.concat("<br>Location: <a href='/location?locationName=" + 
+                quitarEspacios(e.getLocation().getName()) + "'>" + e.getLocation().getName() 
+                + "</a><br></body></html>");
+        return html;
+    }
+    
+    public String festivalToHTML (Festival f) {
+        String html;
+        html = "<html><meta http-equiv=\'Content-Type\' content=\'text/html; charset=UTF-8\' />"
+                + "<head><h1>Festival</h1></head><body>Name: " + f.getName() + "<br>Start date: " 
+                + ((FechaCompleta)f.getStartDate()).fechaToString() + "<br>Start time: " 
+                + ((FechaCompleta)f.getStartTimeFestival()).horaToString() + "h<br>Closing time: " 
+                + ((FechaCompleta)f.getClosingTimeFestival()).horaToString() + "h<br>Closing date: "
+                + ((FechaCompleta)f.getEndingDate()).fechaToString() + "<br>";
+        Iterator i = f.getConcerts().iterator();
+        int j = 1;
+        while (i.hasNext()) {
+            String cName = ((Concert)i.next()).getName();
+            html = html.concat("Concierto " + j + ": <a href='/concert?concertName=" + 
+                    quitarEspacios(cName) + "'>" + cName + "</a><br></body></html>");
+            j++;
+        }
+        return html;
+    }
+    
+    public String quitarEspacios (String enlace) {
+        return enlace.replaceAll(" ", "");
+    }
+    
+    
+}
